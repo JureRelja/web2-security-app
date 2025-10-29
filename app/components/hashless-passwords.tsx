21c4607a7c12fae4726a777e2da75787001f7654
Jure Reljanović <jurereljanovic99@gmail.com>
2025-10-29 15:24:58 +0100

    updated components

--- app/components/hashless-passwords.tsx
+++ app/components/hashless-passwords.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+"use client"
 
-function HashlessPasswords() {
+import { useState } from 'react';
+import { toggleUnsecureDataSaving } from '../actions'
+
+function HashlessPasswords({ enabled }: { enabled: boolean }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleXssSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    setIsLoading(true);
+    await toggleUnsecureDataSaving(!enabled)
+    window.location.reload()
+  }
+    
+  const submitForm = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.currentTarget.closest('form')?.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }))
+  }
+
   return (
-    <div>HashlessPasswords</div>
+    <div>
+      <h1 className="text-4xl font-bold mb-2">Hashless Passwords - krađa tokena unutar kolačića</h1>
+      <form onSubmit={handleXssSubmit}>
+        <input type="checkbox" id="toggle" checked={enabled} onClick={(e) => submitForm(e)} disabled={isLoading} />
+        <label htmlFor="toggle" className="ml-2 text-xl">
+          Ranjivost uključena
+        </label>
+      </form>
+    </div>
   )
 }
 
--- app/components/xss.tsx
+++ app/components/xss.tsx
@@ -1,10 +1,15 @@
 "use client"
+
+import { useState } from "react";
 import { toggleXSS } from "../actions"
 
 export default function XSS({ enabled }: { enabled: boolean }) {
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleXssSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
 
+    setIsLoading(true);
     await toggleXSS(!enabled)
     window.location.reload()
   }
@@ -14,11 +19,14 @@ export default function XSS({ enabled }: { enabled: boolean }) {
   }
   
   return (
-      <div>
-        <h1 className="text-4xl font-bold">Cross-site scripting (XSS) - krađa tokena unutar kolačića</h1>
-        <form onSubmit={handleXssSubmit}>
-          <input type="checkbox" id="toggle" className="peer hidden" onClick={(e) => submitForm(e)} />
-        </form>
-      </div>
+    <div>
+      <h1 className="text-4xl font-bold mb-2">Cross-site scripting (XSS) - krađa tokena unutar kolačića</h1>
+      <form onSubmit={handleXssSubmit}>
+        <input type="checkbox" id="toggle" checked={enabled} onClick={(e) => submitForm(e)} disabled={isLoading} />
+        <label htmlFor="toggle" className="ml-2 text-xl">
+          Ranjivost uključena
+        </label>
+      </form>
+    </div>
   )
 }
