4a0bf2c83eb0b02e356fc1e389e21515b55dce3b
Jure Reljanović <jurereljanovic99@gmail.com>
2025-10-29 17:01:54 +0100

    pg connection fix

--- app/pg-client.ts
+++ app/pg-client.ts
@@ -2,6 +2,6 @@ import { Client } from 'pg';
 
 export function makePgClient() {
   return new Client({
-    ssl: { rejectUnauthorized: false } // allow TLS without verifying CA (common for Render)
+    ssl: { rejectUnauthorized: process.env.NODE_ENV !== 'production' }
   });
 }
